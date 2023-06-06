b1733567662f3ecf94275897cbf2c1e34cbaa3af
Nemolite <g25092011@mail.ru>
2023-06-06 08:46:55 +0300

    map

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,4 +1,11 @@
 @extends('basa')
 @section('contacts')
     <h1>Где нас найти?</h1>
+    <p>Наш адрес: Чувашия, г.Шумерля, ул. Некрасова,д.62</p>
+    <p>Наш email: g25092011@mail.ru</p>
+    <p>Наш тел.: +7-962-598-58-91</p>
+
+    <div class="contacts-map">
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac3ee33e24647f196e34d5ed638da3291284488a9b5c371eaba8e49f5fe303f17&amp;width=650&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+    </div>
 @endsection
